2e301bf57476bb4b50e259e8888f0e238ec80c67
Mark Thanadabouth <mark28ten@gmail.com>
2021-07-21 19:49:55 -0700

    started lab04

--- src/Main.js
+++ src/Main.js
@@ -4,19 +4,41 @@ import CardColumns from 'react-bootstrap/CardColumns';
 import './Main.css';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    
+    }
+  }
+
+  handleChange = e => {
+    const numOfHorns = e.target.value
+    console.log(numOfHorns);
+  }
+
   render() {
-    let itemsToRender = this.props.data.map((beastObjectFromData, index) => <HornedBeast
-      key={index}
-      handleShowModal={this.props.handleShowModal}
-      beast={beastObjectFromData}
-    // title={beastObjectFromData.title}
-    // image_url={beastObjectFromData.image_url}
-    // description={beastObjectFromData.description}
-    />
+    let itemsToRender = this.props.data.map((beastObjectFromData, index) =>
+      <HornedBeast
+        key={index}
+        handleShowModal={this.props.handleShowModal}
+        beast={beastObjectFromData}
+      />
     );
     return (
       <main>
         <h3>Click any image to display the description!</h3>
+        <form>
+          <label>View beasts by the number of horns
+            <select name="horns" id="horns" onChange={this.handleChange}>
+              <option>Select an Option</option>
+              <option value="all">All</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="100">100</option>
+            </select>
+          </label>
+        </form>
         <CardColumns>
           {itemsToRender}
         </CardColumns>
