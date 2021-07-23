8e8d5d6147a01def0e4fd553c808e2d5e6aa8a23
Mark Thanadabouth <mark28ten@gmail.com>
2021-07-22 20:21:36 -0700

    finished lab04

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 import CardColumns from 'react-bootstrap/CardColumns';
+import Form from 'react-bootstrap/Form';
 import './Main.css';
 
 class Main extends React.Component {
-
-  handleChange = e => {
-    const numOfHorns = parseInt(e.target.value);
-    if (numOfHorns) {
-      this.props.data.filter(beast => beast.horns === numOfHorns);
+  constructor(props) {
+    super(props)
+    this.state = {
+      beastData: this.props.data,
     }
-    // Filter over the data for each indv. beast
-    // let filteredItems = 
-    // Comparing the beast horns to the value of the onChange
-    // console.log(numOfHorns);
+  }
+  handleChange = e => {
+    let beastData = this.props.data
+    let numOfHorns = parseInt(e.target.value);
+    if (numOfHorns) {
+      beastData = beastData.filter(beast => beast.horns === numOfHorns);
+    }
+    this.setState({
+      beastData: beastData,
+    })
   }
 
   render() {
-    let itemsToRender = this.props.data.map((beastObjectFromData, index) =>
+    let itemsToRender = this.state.beastData.map((beastObjectFromData, index) =>
       <HornedBeast
         key={index}
         handleShowModal={this.props.handleShowModal}
@@ -27,18 +33,18 @@ class Main extends React.Component {
     return (
       <main>
         <h3>Click any image to display the description!</h3>
-        <form>
-          <label>View beasts by the number of horns
-            <select name="horns" id="horns" onChange={this.handleChange}>
+        <Form>
+          <Form.Label>View beasts by the number of horns!
+            <Form.Control as="select" custom onChange={this.handleChange}>
               <option>Select an Option</option>
-              <option value="all">All</option>
+              <option value="0">All</option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
               <option value="100">100</option>
-            </select>
-          </label>
-        </form>
+            </Form.Control>
+          </Form.Label>
+        </Form>
         <CardColumns>
           {itemsToRender}
         </CardColumns>
